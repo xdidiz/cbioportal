36272228ec253ef582d21c256196de6b9a3bd737
Adam Abeshouse <adama@cbio.mskcc.org>
2014-10-01 18:17:32 -0400

    more angular, promises

--- portal/src/main/webapp/js/src/query-page/query_page.js
+++ portal/src/main/webapp/js/src/query-page/query_page.js
@@ -1,37 +1,51 @@
 var app = angular.module('query-page-module',[]);
-app.factory('Global', ['$http', function($http) {
-    var vars = {metaDataJson:-1, cancer_study_id:"all",case_set_id:"-1", case_sets:[], gene_set_id:"user-defined-list", oql_query:""};
-    var _updateCaseSets = function() {
-            console.log("UPDATING CASE SETS");
-            var ret = [];
-            for (var i=0; i<vars.metaDataJson.cancer_studies[vars.cancer_study_id].case_sets.length; i++) {
-                var set = vars.metaDataJson.cancer_studies[vars.cancer_study_id].case_sets[i];
-                ret.push({id:set.id, label:set.name+' ( '+set.size+' ) '});
-            }
-            ret.push({id:'-1',label:'User-defined Case List'});
-            vars.case_sets = ret;
-        };
-        
+app.factory('Global', ['$http', '$q', function($http, $q) {
+    var vars = {metaDataJson:-1, cancer_study_id:"all",case_set_id:"-1",  gene_set_id:"user-defined-list", oql_query:""};
+    
     return {
         vars: function() {
             return vars;
         },
-        loadCurrentStudy: function() {
-            var id = vars.cancer_study_id;
-            $http({method:'GET', url:'/portal_meta_data.json?study_id='+id}).
-                success(function(data,status,headers,config) {
-                    vars.metaDataJson.cancer_studies[id] = data;
-                    _updateCaseSets();
-            });
+        metaDataJson: function() {
+            var q = $q.defer();
+            if (vars.metaDataJson === -1) {
+                $http({method: 'GET', url: '/portal_meta_data.json?partial_studies=true&partial_genesets=true'}).
+                        success(function(data, status, headers, config) {
+                            vars.metaDataJson = data;
+                            q.resolve(vars.metaDataJson);
+                        });
+            } else {
+                q.resolve(vars.metaDataJson);
+            }
+            return q.promise;
         },
-        loadStudy: function(id) {
-            $http({method:'GET', url:'/portal_meta_data.json?study_id='+id}).
-                success(function(data,status,headers,config) {
-                    vars.metaDataJson.cancer_studies[id] = data;
-                    _updateCaseSets();
-            });
+        study: function(id) {
+            var q = $q.defer();
+            if (vars.metaDataJson.cancer_studies[id].partial === 'true') {
+                 $http({method:'GET', url:'/portal_meta_data.json?study_id='+id}).
+                    success(function(data,status,headers,config) {
+                        vars.metaDataJson.cancer_studies[id] = data;
+                        q.resolve(vars.metaDataJson.cancer_studies[id]);
+                    });        
+            } else {
+                q.resolve(vars.metaDataJson.cancer_studies[id]);
+            }
+            return q.promise;
         },
-        updateCaseSets: _updateCaseSets
+        geneList: function(id) {
+            var q = $q.defer();
+            if (vars.metaDataJson.gene_sets[id].gene_list === "") {
+                $http({method:'GET', url:'/portal_meta_data.json?geneset_id='+id.replace(/\//g,'')}).
+                    success(function(data,status,headers,config) {
+                        vars.metaDataJson.gene_sets[id].gene_list = data.list.split(/\s/).join("; ")
+                        q.resolve(vars.metaDataJson.gene_sets[id].gene_list);
+                    });
+            } else {
+                q.resolve(vars.metaDataJson.gene_sets[id].gene_list);
+            }
+            return q.promise;
+        },
+        
     };
     }]);
 
@@ -44,43 +58,48 @@ app.filter('to_trusted', ['$sce', function($sce) {
 app.controller('mainController', ['$scope', 'Global', '$http', function($scope, Global, $http) {
         $scope.vars = Global.vars();
         angular.element(document).ready(function() {
-            $http({method: 'GET', url: '/portal_meta_data.json?partial_studies=true&partial_genesets=true'}).
-                    success(function(data, status, headers, config) {
-                        $scope.vars.metaDataJson = data;
-                        Global.loadCurrentStudy();
-                    });
+            //load metadata
+            Global.metaDataJson().then(function() {
+                // do init stuff here
+            });
         });
 }]);
 
-app.controller('step1Controller', ['$http','$scope', 'Global', function($http,$scope, Global) {
+
+app.controller('step123Controller',['$http','$scope', 'Global', function($http,$scope, Global) {
+        $scope.dataPriorities = [{id:"pri_mutcna",label:"Mutation and CNA"},{id:"pri_mut",label:"Only Mutation"},{id:"pri_cna",label:"Only CNA"}];
         $scope.updateStudy = function() {
             // load if necessary
-            if ($scope.vars.metaDataJson.cancer_studies[$scope.vars.cancer_study_id].partial === 'true' ) {
-                Global.loadStudy($scope.vars.cancer_study_id);
-            } else {
-                // just update case sets
-                Global.updateCaseSets();
-            }
+            Global.metaDataJson().then(function() {
+                Global.study($scope.vars.cancer_study_id).then(function() {
+                    $scope.updateCaseSets();
+                });
+            });
         };
-        
+        $scope.updateCaseSets = function() {
+            var ret = [];
+            for (var i=0; i<$scope.vars.metaDataJson.cancer_studies[$scope.vars.cancer_study_id].case_sets.length; i++) {
+                var set = $scope.vars.metaDataJson.cancer_studies[$scope.vars.cancer_study_id].case_sets[i];
+                ret.push({id:set.id, label:set.name+' ('+set.size+') '});
+            }
+            ret.push({id:'-1',label:'User-defined Case List'});
+            $scope.caseSets = ret;
+        };
         $scope.onChange = function() {
             $scope.updateStudy();
         };
-}]);
+        $scope.profileGroups = [];
+        $scope.caseSets = [];
+}]);  
 
-app.controller('step2CrossController', ['$scope', 'Global', function($scope, Global) {
-        $scope.dataPriorities = [{id:"pri_mutcna",label:"Mutation and CNA"},{id:"pri_mut",label:"Only Mutation"},{id:"pri_cna",label:"Only CNA"}];
-        }]);
-    
-app.controller('step2Controller', ['$scope', 'Global', function($scope, Global) {
-        }]);    
-app.controller('step3Controller', ['$scope', 'Global', function($scope, Global) {
-        
-        }]);    
 app.controller('step4Controller', ['$scope', 'Global', function($scope, Global) {
-        var updateOqlQuery = function() {
-            console.log("yo");
-            $scope.vars.oql_query = "yo";
+        $scope.updateOqlQuery = function() {
+            //load if necessary
+            Global.metaDataJson().then(function() {
+                Global.geneList($scope.vars.gene_set_id).then(function(json) {
+                    $scope.vars.oql_query = $scope.vars.metaDataJson.gene_sets[$scope.vars.gene_set_id].gene_list;
+                });
+            });
         };
         }]);    
         
--- portal/src/main/webapp/query_page.jsp
+++ portal/src/main/webapp/query_page.jsp
@@ -11,35 +11,40 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>
-        <div ng-controller="step1Controller">
-            <h2>Select Cancer Study:</h2>
-            <select ng-model="vars.cancer_study_id" 
-                    ng-options="csid as csobj.name group by vars.metaDataJson.type_of_cancers[csobj.type_of_cancer] for (csid, csobj) in vars.metaDataJson.cancer_studies"
-                    ng-change="onChange()"
-                    >
-            </select>
-            <p ng-hide="vars.cancer_study_id==='all'" ng-bind-html="vars.metaDataJson.cancer_studies[vars.cancer_study_id].description | to_trusted"></p>
-            <button>Study Summary</button>
-        </div>
-        <div ng-controller="step2CrossController" ng-show="vars.cancer_study_id === 'all'">
-            <h2>Select Data Type Priority:</h2>
-            <label ng-repeat="datap in dataPriorities">
-                <input type="radio" name="data_priority" ng-model="vars.data_priority" value="{{datap.id}}">{{datap.label}}
-            </label>
-        </div>
-        <div ng-controller="step2Controller" ng-hide="vars.cancer_study_id === 'all'">
-            <h2>Select Genomic Profiles:</h2>
-            
-        </div>
-        <div ng-controller="step3Controller" ng-hide="vars.cancer_study_id === 'all'">
-            <h2>Select Patient/Case Set:</h2>
-            <select ng-model="vars.case_set_id"
-                    ng-options="cs.id as cs.label for cs in vars.case_sets"
-                    >
-            </select>
-            <div ng-show="vars.case_set_id === '-1'">
-                <p>Enter case IDs below:</p>
-                <textarea ng-model="vars.custom_case_list" rows="6" cols="80"></textarea>
+        <div ng-controller="step123Controller">
+            <div>
+                <h2>Select Cancer Study:</h2>
+                <select ng-model="vars.cancer_study_id" 
+                        ng-options="csid as csobj.name group by vars.metaDataJson.type_of_cancers[csobj.type_of_cancer] for (csid, csobj) in vars.metaDataJson.cancer_studies"
+                        ng-change="onChange()"
+                        >
+                </select>
+                <p ng-hide="vars.cancer_study_id==='all'" ng-bind-html="vars.metaDataJson.cancer_studies[vars.cancer_study_id].description | to_trusted"></p>
+                <button>Study Summary</button>
+            </div>
+            <div ng-show="vars.cancer_study_id === 'all'">
+                <h2>Select Data Type Priority:</h2>
+                <label ng-repeat="datap in dataPriorities">
+                    <input type="radio" name="data_priority" ng-model="vars.data_priority" value="{{datap.id}}">{{datap.label}}
+                </label>
+            </div>
+            <div ng-hide="vars.cancer_study_id === 'all'">
+                <h2>Select Genomic Profiles:</h2>
+                <div ng-repeat="prof in profiles">
+                    <div profile-group="prof">
+                    </div>
+                </div>
+            </div>
+            <div ng-hide="vars.cancer_study_id === 'all'">
+                <h2>Select Patient/Case Set:</h2>
+                <select ng-model="vars.case_set_id"
+                        ng-options="cs.id as cs.label for cs in caseSets"
+                        >
+                </select>
+                <div ng-show="vars.case_set_id === '-1'">
+                    <p>Enter case IDs below:</p>
+                    <textarea ng-model="vars.custom_case_list" rows="6" cols="80"></textarea>
+                </div>
             </div>
         </div>
         <div ng-controller="step4Controller">
@@ -51,15 +56,13 @@
                     >
             </select>
             <textarea ng-model="vars.oql_query" rows="6" cols="80" placeholder="Enter HUGO Gene Symbols or Gene Aliases"></textarea>
-            <p>{{vars.oql_query}}</p>
         </div>
+        <button type="button">Submit</button>
         <p>The selected cancer study is {{vars.cancer_study_id}}</p><br>
         <p ng-show="vars.cancer_study_id === 'all'">The selected data priority is {{vars.data_priority}}</p><br>
-        <!--<p ng-hide="vars.cancer_study_id === 'all'">The selected genomic profiles are {{vars.data_priority}}</p><br>-->
+        <p ng-hide="vars.cancer_study_id === 'all'">The selected genomic profiles are {{vars.data_priority}}</p><br>
         <p>The selected case set is {{vars.case_set_id}}</p>
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js"></script>
         <script type="text/javascript" src="js/src/query-page/query_page.js"></script>
     </body>
 </html>
-
-<!--ng-options="cs.name group by $window.type_of_cancers[cs.type_of_cancer] for cs in $window.cancer_studies">-->
