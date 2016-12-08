f7a26c17decc541702dee97ab7ca1ab1cf41ebe5
Fedde Schaeffer <fedde@thehyve.nl>
2016-12-08 19:04:59 +0100

    Add geneset tracks to the Oncoprint init routines
    Adding calls that will be necessary to initialise and populate the
    tracks.

--- portal/src/main/webapp/js/src/oncoprint/setup.js
+++ portal/src/main/webapp/js/src/oncoprint/setup.js
@@ -647,6 +647,7 @@ window.CreateCBioPortalOncoprintWithToolbar = function (ctr_selector, toolbar_se
 			
 			var total_tracks_to_add = Object.keys(State.genetic_alteration_tracks).length
 						+ heatmap_data.length
+						+ Object.keys(State.geneset_tracks).length
 						+ Object.keys(State.clinical_tracks).length;
 			
 			utils.timeoutSeparatedLoop(Object.keys(State.genetic_alteration_tracks), function (track_line, i) {
@@ -672,7 +673,26 @@ window.CreateCBioPortalOncoprintWithToolbar = function (ctr_selector, toolbar_se
 					oncoprint.setTrackData(track_id, heatmap_track_data.oncoprint_data, 'uid');
 					oncoprint.setTrackTooltipFn(track_id, tooltip_utils.makeHeatmapTrackTooltip(heatmap_track_data.genetic_alteration_type, 'sample', true));
 					LoadingBar.update((i + Object.keys(State.genetic_alteration_tracks).length) / total_tracks_to_add);
+				});
+			}).then(function () {
+			    return QuerySession.getSelectedGsvaProfile();
+			}).then(function (gsva_profile) {
+			    if (gsva_profile !== null) {
+				console.log("in populateSampleData, calling QuerySession.getSampleGsvaData()");
+				return QuerySession.getSampleGsvaData()
+				.then(function (gsva_data_by_line) {
+				    return utils.timeoutSeparatedLoop(Object.keys(State.geneset_tracks), function(gs_line, i) {
+					console.log("geneset data retrieved, populating sample data for track " + gsva_data_by_line[gs_line].hugo_gene_symbol);
+					var gs_id = State.geneset_tracks[gs_line];
+					oncoprint.setTrackData(gs_id, gsva_data_by_line[gs_line].oncoprint_data, 'uid');
+					// TODO: use something more appropriate than makeHeatmapTrackTooltip
+					oncoprint.setTrackTooltipFn(gs_id, tooltip_utils.makeHeatmapTrackTooltip(gsva_profile, 'sample', true));
+					LoadingBar.update((i +
+						heatmap_data.length +
+						Object.keys(State.genetic_alteration_tracks).length) / total_tracks_to_add);
 				    });
+				});
+			    }
 			}).then(function () {
 			    return utils.timeoutSeparatedLoop(Object.keys(State.clinical_tracks), function(track_id, i) {
 				var attr = State.clinical_tracks[track_id];
@@ -680,7 +700,10 @@ window.CreateCBioPortalOncoprintWithToolbar = function (ctr_selector, toolbar_se
 				oncoprint.setTrackData(track_id, clinical_data[attr.attr_id], 'uid');
 				oncoprint.setTrackTooltipFn(track_id, tooltip_utils.makeClinicalTrackTooltip(attr, 'sample', true));
 				oncoprint.setTrackInfo(track_id, "");
-				LoadingBar.update((i + heatmap_data.length + Object.keys(State.genetic_alteration_tracks).length) / total_tracks_to_add);
+				LoadingBar.update((i +
+					Object.keys(State.geneset_tracks).length +
+					heatmap_data.length +
+					Object.keys(State.genetic_alteration_tracks).length) / total_tracks_to_add);
 			    });
 			}).then(function () {
 			    console.log("sample data populated, releasing rendering");
@@ -727,6 +750,7 @@ window.CreateCBioPortalOncoprintWithToolbar = function (ctr_selector, toolbar_se
 			
 			var total_tracks_to_add = Object.keys(State.genetic_alteration_tracks).length
 						+ heatmap_data.length
+						+ Object.keys(State.geneset_tracks).length
 						+ Object.keys(State.clinical_tracks).length;
 			
 			utils.timeoutSeparatedLoop(Object.keys(State.genetic_alteration_tracks), function (track_line, i) {
@@ -754,13 +778,35 @@ window.CreateCBioPortalOncoprintWithToolbar = function (ctr_selector, toolbar_se
 					LoadingBar.update((i + Object.keys(State.genetic_alteration_tracks).length) / total_tracks_to_add);
 				    });
 			}).then(function () {
+			    return QuerySession.getSelectedGsvaProfile();
+			}).then(function (gsva_profile) {
+			    if (gsva_profile !== null) {
+				console.log("in populatePatientData, calling QuerySession.getPatientGsvaData()");
+				return QuerySession.getPatientGsvaData()
+				.then(function (gsva_data_by_line) {
+				    return utils.timeoutSeparatedLoop(Object.keys(State.geneset_tracks), function(gs_line, i) {
+					console.log("geneset data retrieved, populating sample data for track " + gsva_data_by_line[gs_line].hugo_gene_symbol);
+					var gs_id = State.geneset_tracks[gs_line];
+					oncoprint.setTrackData(gs_id, gsva_data_by_line[gs_line].oncoprint_data, 'uid');
+					// TODO: use something more appropriate than makeHeatmapTrackTooltip
+					oncoprint.setTrackTooltipFn(gs_id, tooltip_utils.makeHeatmapTrackTooltip(gsva_profile, 'patient', true));
+					LoadingBar.update((i +
+						heatmap_data.length +
+						Object.keys(State.genetic_alteration_tracks).length) / total_tracks_to_add);
+				    });
+				});
+			    }
+			}).then(function () {
 			    return utils.timeoutSeparatedLoop(Object.keys(State.clinical_tracks), function(track_id, i) {
 				var attr = State.clinical_tracks[track_id];
 				console.log("populating patient data for clinical track " + attr.attr_id);
 				oncoprint.setTrackData(track_id, clinical_data[attr.attr_id], 'uid');
 				oncoprint.setTrackTooltipFn(track_id, tooltip_utils.makeClinicalTrackTooltip(attr, 'patient', true));
 				oncoprint.setTrackInfo(track_id, "");
-				LoadingBar.update((i + heatmap_data.length + Object.keys(State.genetic_alteration_tracks).length) / total_tracks_to_add);
+				LoadingBar.update((i +
+					Object.keys(State.geneset_tracks).length +
+					heatmap_data.length +
+					Object.keys(State.genetic_alteration_tracks).length) / total_tracks_to_add);
 			    });
 			}).then(function () {
 			    console.log("patient data populated");
@@ -1560,6 +1606,20 @@ window.CreateCBioPortalOncoprintWithToolbar = function (ctr_selector, toolbar_se
 	    });
 	    var ret = $.when.apply(null, promises);
 	    return ret;
+	}).then(function () {
+	    return QuerySession.getSelectedGsvaProfile();
+	}).then(function (gsva_profile) {
+	    if (gsva_profile !== null) {
+		console.log("in initOncoprint, fetching GSVA data");
+		return QuerySession.getSampleGsvaData()
+		.then(function (gsva_data) {
+		    console.log("in initOncoprint, GSVA data fetched, adding tracks");
+		    State.addGsvaTracks(gsva_data);
+		})
+	    } else {
+		// gsva not applicable, return a resolved Promise
+		return $.when();
+	    }
 	}).then(function fetchClinicalAttributes() {
 	    console.log("in initOncoprint, fetching clinical attributes");
 	    QuerySession.getClinicalAttributes()
