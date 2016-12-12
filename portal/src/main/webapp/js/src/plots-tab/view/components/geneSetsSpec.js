b7f997b324fa9fc847af88e0de0861854c7964b7
Sander Tan <sandertan@thehyve.nl>
2016-12-12 16:05:29 +0100

    Added copyright and authors

--- portal/src/main/webapp/js/src/plots-tab/view/components/geneSetsSpec.js
+++ portal/src/main/webapp/js/src/plots-tab/view/components/geneSetsSpec.js
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2016 The Hyve B.V.
+ * This code is licensed under the GNU Affero General Public License,
+ * version 3, or (at your option) any later version.
+ *
+ * This file is part of cBioPortal.
+ *
+ * cBioPortal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Oleguer Plantalech and Sander Tan, The Hyve
+// December 2016
+
 var geneSetsSpec = (function() {
 
     function appendGeneSetsList(axis) {
@@ -28,7 +52,6 @@ var geneSetsSpec = (function() {
                 }
             }
         });
-
     }
 
     function appendPlotValueList(axis) {
@@ -44,8 +67,7 @@ var geneSetsSpec = (function() {
 
         $("#" + ids.sidebar[axis].profile_name).change(function() {
             regenerate_plots(axis);
-        });
-        
+        });    
     }
     
     function updatePlotValueList(axis) {
@@ -58,7 +80,6 @@ var geneSetsSpec = (function() {
                           "<option value='" + obj.id + "'>" + obj.name + "</option>");
                 
             });
-
         };
 
         $("#" + ids.sidebar[axis].profile_name).change(function() {
