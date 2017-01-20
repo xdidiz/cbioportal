f73d80b95c261f6cd9e964ce94c44d70f4e27294
oplantalech <oleguer@thehyve.nl>
2017-01-20 12:25:24 +0100

    Transform NCBI build, genomic build and species into global variables and update gene length code

--- core/src/main/java/org/mskcc/cbio/portal/dao/DaoGene.java
+++ core/src/main/java/org/mskcc/cbio/portal/dao/DaoGene.java
@@ -33,6 +33,7 @@
 package org.mskcc.cbio.portal.dao;
 
 import org.mskcc.cbio.portal.model.CanonicalGene;
+import org.mskcc.cbio.portal.util.ProgressMonitor;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -41,6 +42,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -70,6 +72,49 @@ final class DaoGene {
         return addGene(gene);
     }
 
+    /**
+     * Update Gene Record in the Database. 
+     */
+    public static int updateGene(CanonicalGene gene) throws DaoException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        boolean setBulkLoadAtEnd = false;
+        try {
+            //this method only works well with bulk load off, especially 
+            //when it is called in a process that may update a gene more than once
+            //(e.g. the ImportGeneData updates some of the fields based on one 
+            // input file and other fields based on another input file):
+            setBulkLoadAtEnd = MySQLbulkLoader.isBulkLoad();
+            MySQLbulkLoader.bulkLoadOff();
+            
+            int rows = 0;
+            con = JdbcUtil.getDbConnection(DaoGene.class);
+            pstmt = con.prepareStatement
+                    ("UPDATE gene SET `HUGO_GENE_SYMBOL`=?, `TYPE`=?,`CYTOBAND`=?,`LENGTH`=? WHERE `ENTREZ_GENE_ID`=?");
+            pstmt.setString(1, gene.getHugoGeneSymbolAllCaps());
+            pstmt.setString(2, gene.getType());
+            pstmt.setString(3, gene.getCytoband());
+            pstmt.setInt(4, gene.getLength());
+            pstmt.setLong(5, gene.getEntrezGeneId());
+            rows += pstmt.executeUpdate();
+            if (rows != 1) {
+                ProgressMonitor.logWarning("No change for " + gene.getEntrezGeneId() + " " + gene.getHugoGeneSymbolAllCaps() + "? Code " + rows);
+            }
+
+            return rows;
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        } finally {
+            if (setBulkLoadAtEnd) {
+                //reset to original state:
+                MySQLbulkLoader.bulkLoadOn();
+            }   
+            JdbcUtil.closeAll(DaoGene.class, con, pstmt, rs);
+        }
+        
+}
+    
     /**
      * Adds a new Gene Record to the Database.
      *
@@ -150,6 +195,7 @@ final class DaoGene {
 
             return rows;
         } catch (SQLException e) {
+        	System.out.println("wrong: " + gene.getHugoGeneSymbolAllCaps());
             throw new DaoException(e);
         } finally {
             JdbcUtil.closeAll(DaoGene.class, con, pstmt, rs);
@@ -239,7 +285,7 @@ final class DaoGene {
             JdbcUtil.closeAll(DaoGene.class, con, pstmt, rs);
         }
     }
-
+    
     /**
      * Gets all Genes in the Database.
      *
@@ -348,7 +394,9 @@ final class DaoGene {
      * @param entrezGeneId 
      */
     public static void deleteGene(long entrezGeneId) throws DaoException {
-        Connection con = null;
+        deleteGeneAlias(entrezGeneId);
+        
+    	Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
         try {
@@ -361,8 +409,6 @@ final class DaoGene {
         } finally {
             JdbcUtil.closeAll(DaoGene.class, con, pstmt, rs);
         }
-        
-        deleteGeneAlias(entrezGeneId);
     }
     
     /**
@@ -389,6 +435,8 @@ final class DaoGene {
      * Deletes all Gene Records in the Database.
      *
      * @throws DaoException Database Error.
+     * 
+     * @deprecated only used by deprecated code, so deprecating this as well.
      */
     public static void deleteAllRecords() throws DaoException {
         Connection con = null;
@@ -408,6 +456,12 @@ final class DaoGene {
         deleteAllAliasRecords();
     }
     
+    /**
+     * 
+     * @throws DaoException
+     * 
+     * @deprecated only used by deprecated code, so deprecating this as well.
+     */
     private static void deleteAllAliasRecords() throws DaoException {
         Connection con = null;
         PreparedStatement pstmt = null;
