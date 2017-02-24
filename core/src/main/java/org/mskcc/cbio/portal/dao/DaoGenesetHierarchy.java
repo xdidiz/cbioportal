20036871af657a2bc26e26184f8746c29572eb9f
Sander Tan <sandertan@thehyve.nl>
2017-02-24 17:51:53 +0100

    Update importer code for Gene Set data loading
    * Create importer for gene set data and supplementary gene set data
    * Create importer for gene set hierarchy data
    * Update `ImportTabDelimData.java` to support import of GSVA study data
    * Update validator to support validation of GSVA study data
    * integration test includes GSVA data
    * Create unit tests for gene set importer
    * Create unit tests for gene set hierarchy importer
    * Create unit tests for validation of GSVA data
    * Update study_es_0 with GSVA and Z-Score data
    * Add documentation
    * Rename old GeneSet class to SetOfGenes
    
    Authors: Angelica Ochoa <aochoa4230@gmail.com>
             Sander Tan <sandertan@thehyve.nl>
    
    integration test includes GSVA data
    
    added test for GSVA data loading

---
+++ core/src/main/java/org/mskcc/cbio/portal/dao/DaoGenesetHierarchy.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2017 The Hyve B.V.
+ * This code is licensed under the GNU Affero General Public License (AGPL),
+ * version 3, or (at your option) any later version.
+ */
+
+/*
+ * This file is part of cBioPortal.
+ *
+ * cBioPortal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ * @author Sander Tan
+*/
+
+package org.mskcc.cbio.portal.dao;
+
+import java.sql.*;
+
+import org.mskcc.cbio.portal.model.GenesetHierarchy;
+
+public class DaoGenesetHierarchy {
+	
+	// Keep Constructor empty
+	private DaoGenesetHierarchy() {
+	}
+    
+	/**
+     * Add gene set hierarchy object to geneset_hierarchy table in database.
+     * @throws DaoException 
+     */
+	public static void addGenesetHierarchy(GenesetHierarchy genesetHierarchy) throws DaoException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        
+        try {
+        	// Open connection to database
+            connection = JdbcUtil.getDbConnection(DaoGenesetHierarchy.class);
+	        
+	        // Prepare SQL statement
+            preparedStatement = connection.prepareStatement("INSERT INTO geneset_hierarchy " 
+	                + "(`NODE_NAME`, `PARENT_ID`) VALUES(?,?)", Statement.RETURN_GENERATED_KEYS);
+	        
+            // Fill in statement
+            preparedStatement.setString(1, genesetHierarchy.getNodeName());
+            
+            if (genesetHierarchy.getParentId() == 0) {
+                preparedStatement.setNull(2, java.sql.Types.INTEGER);
+            } else {
+                preparedStatement.setInt(2, genesetHierarchy.getParentId());
+            }
+            
+            // Execute statement
+            preparedStatement.executeUpdate();
+
+            // Get the auto generated key, which is the Node ID:
+            resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+            	genesetHierarchy.setNodeId(resultSet.getInt(1));	
+            }       
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        } finally {
+            JdbcUtil.closeAll(DaoGenesetHierarchy.class, connection, preparedStatement, resultSet);
+        }
+	}
+
+
+    /**
+     * Retrieve gene set hierarchy object from geneset_hierarchy table in database to check if table if filled.
+     * @throws DaoException 
+     */
+	public static boolean checkGenesetHierarchy() throws DaoException {
+		Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        
+        try {
+        	// Open connection to database
+            connection = JdbcUtil.getDbConnection(DaoGenesetHierarchy.class);
+	        
+	        // Prepare SQL statement
+            preparedStatement = connection.prepareStatement("SELECT * FROM geneset_hierarchy LIMIT 1");
+            
+            // Execute statement
+            resultSet = preparedStatement.executeQuery();
+            
+            // return null if result set is empty
+            if (resultSet.next()){
+            
+            	//ResultSet is filled
+            	return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        } finally {
+            JdbcUtil.closeAll(DaoGenesetHierarchy.class, connection, preparedStatement, resultSet);
+        }
+	}
+
+
+    /**
+     * Deletes all records from 'geneset_hierarchy_parent' table in database
+     * @throws DaoException 
+     */   
+	public static void deleteAllGenesetHierarchyRecords() throws DaoException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+        	connection = JdbcUtil.getDbConnection(DaoGenesetHierarchy.class);
+        	preparedStatement = connection.prepareStatement("DELETE FROM geneset_hierarchy");
+        	preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        } finally {
+            JdbcUtil.closeAll(DaoGenesetHierarchy.class, connection, preparedStatement, resultSet);
+        }
+    }
+    
+}
